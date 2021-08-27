188afb5c6bb0966c4f928302721a2af5b1948068
hsg0830 <hsg0830@gmail.com>
2021-08-27 16:49:48 +0900

    辞書検索リンク追加

---
+++ resources/views/commons/side-dic.blade.php
@@ -0,0 +1,17 @@
+<div class="top-search">
+  <div class="top-search__heading">조선말대사전검색</div>
+  <div class="top-search__box">
+    <div name="frmSearch" class="top-search__title" id="search-box-a">
+      <form action="https://dprktoday.com/kor_dic/index.php?page=1" method="POST" target="_blank" class="top-search__form">
+        <input type="hidden" id="hdn_option" name="dictype" value="KR">
+				<input type="hidden" name="cmd" value="search">
+        <div class="top-search__select">
+          <input type="search" name="keyword" id="word"/>
+        </div>
+        <div class="top-search__submit">
+          <input type="submit" value="검색" class="btn global-btn" />
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
